94b68c79b8a5a3eb1403f4920b733609b4f53d07
Rahmad Noor Ikhsan <rajaikhsan2021@gmail.com>
2023-03-05 13:58:18 +0700

    Initial commit

---
+++ app/src/main/java/com/ikhsan/learnactionbar/MenuActivity.kt
@@ -0,0 +1,17 @@
+package com.ikhsan.learnactionbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.ikhsan.learnactionbar.databinding.ActivityMainBinding
+
+class MenuActivity : AppCompatActivity() {
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
+
